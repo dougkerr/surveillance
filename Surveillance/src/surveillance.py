b1c681f45cdaa019c78dfc1a0a041c0d9f48f0b4
Douglas Kerr <dougk@halekerr.com>
2014-02-13 14:54:08 -0800

    Add detailed logging for crop_image() failure.

--- Surveillance/src/surveillance.py
+++ Surveillance/src/surveillance.py
@@ -151,7 +151,8 @@ def crop_image(img, croparea):
     try:
         cropped_image = img.crop((topleft_x, topleft_y, lowerright_x, lowerright_y))
     except IOError:
-        logging.error("crop_image: Can't crop")
+        logging.error("crop_image: Can't crop. Img size: (%d, %d); crop area topleft: (%d, %d), lowerright (%d, %d)" \
+                      % size_x, size_y, topleft_x, topleft_y, lowerright_x, lowerright_y)
         cropped_image = None
 
     return cropped_image
