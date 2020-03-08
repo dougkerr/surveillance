782883a7cc602810a786ce7bcd3013d9b20bc3af
Douglas Kerr <dougk@halekerr.com>
2020-03-08 12:50:56 -0700

    Add kswapd0hack unit tests
    - Make kswapd0hack.sh unit test friendly.

--- CommunityView/kswapd0hack/kswapd0hack.sh
+++ CommunityView/kswapd0hack/kswapd0hack.sh
@@ -1,27 +1,63 @@
+#!/bin/sh
+################################################################################
+#
+# Copyright (C) 2019 Neighborhood Guard, Inc.  All rights reserved.
+# Original author: Douglas Kerr
+# 
+# This file is part of CommunityView.
+# 
+# CommunityView is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# CommunityView is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+# 
+# You should have received a copy of the GNU Affero General Public License
+# along with FTP_Upload.  If not, see <http://www.gnu.org/licenses/>.
+#
+################################################################################
+
 # If kswapd0 uses over 10% of the CPU, reboot.
 # Check every ten minutes.
 
-while true
-do
-    kswappct=`ps -C kswapd0 -o '%cpu' --no-header`
-    #kswappct=`ps -C python -o '%cpu' --no-header`
-    if expr $kswappct '>' 1 > /dev/null
+logfile=/var/opt/communityview/kswapd0hack.log
+
+ckcpu() {
+    kspct=`ps -C kswapd0 -o '%cpu' --no-header`
+
+    # get integer portion of CPU percentage
+    kspct_i=`echo "$kspct" | sed 's/ *\([0-9]*\).*/\1/'`
+
+    if expr $kspct_i '>=' 1 > /dev/null
     then
         echo `date --iso-8601=seconds` \
-            "kswapd0 using $kswappct% of CPU." \
-            >> /var/opt/communityview/log/kswapd0hack.log
+            "kswapd0 using" $kspct"% of CPU." \
+            >> "$logfile"
         echo `date --iso-8601=seconds` `uptime` \
-            >> /var/opt/communityview/log/kswapd0hack.log
+            >> "$logfile"
     fi
-    if expr $kswappct '>' 10 > /dev/null
+    if expr $kspct_i '>=' 10 > /dev/null
     then
         echo `date --iso-8601=seconds` \
-            "kswapd0 using $kswappct% of CPU. Rebooting." \
-            >> /var/opt/communityview/log/kswapd0hack.log
+            "kswapd0 using" $kspct"% of CPU. Rebooting." \
+            >> "$logfile"
         echo `date --iso-8601=seconds` `uptime` \
-            >> /var/opt/communityview/log/kswapd0hack.log
-        ps ax >> /var/opt/communityview/log/kswapd0hack.log
-        shutdown -r
+            >> "$logfile"
+        ps auxww >> "$logfile"
+        mv "$logfile" "$logfile.old"
+        shutdown -r now
     fi
-    sleep 600
-done
+}
+
+if [ ! UNIT_TEST_IN_PROGRESS ]
+then
+    while true
+    do
+        ckcpu
+        sleep 600
+    done
+fi
